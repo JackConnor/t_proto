b5489219eb989db428398fba3c1d6ec97bdb4a60
Jack Connor <jack.connor83@gmail.com>
2018-11-19 08:05:34 -0800

    initial listening flow architecture added

--- scripts.js
+++ scripts.js
@@ -1,1 +1,46 @@
 console.log('booom');
+
+class App {
+  constructor() {
+    this.config();
+  }
+
+  setupEvents() {
+    const initEl = document.getElementsByClassName('initial-message-container')[0];
+    initEl.addEventListener('click', (evt) => {
+      evt.currentTarget.classList.remove('show-container');
+      this.launchListenMod();
+    });
+
+    const listenEl = document.getElementsByClassName('listen-container')[0];
+    listenEl.addEventListener('click', (evt) => {
+      evt.currentTarget.classList.remove('show-container');
+      this.giveOptionsMod();
+    });
+  }
+
+  launchListenMod() {
+    const listenEl = document.getElementsByClassName('listen-container')[0];
+    listenEl.classList.add('show-container');
+    setInterval(() => {
+      if (!listenEl.classList.contains('listen-second-color')) {
+        listenEl.classList.add('listen-second-color');
+      }
+      else {
+        listenEl.classList.remove('listen-second-color');
+      }
+    }, 1000);
+  }
+
+  giveOptionsMod() {
+    const optionsEl = document.getElementsByClassName('options-container ')[0];
+    optionsEl.classList.add('show-container');
+  }
+
+  config () {
+    console.log('booom');
+    this.setupEvents();
+  }
+}
+
+const app = new App();
